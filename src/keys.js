767a096e618f4c2b013bc1c2ac1f45465c08be16
Primal <alexis200123@outlook.es>
2022-04-21 02:17:15 -0500

    otra vez pero con nodejs

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database:{
-        host: 'localhost',
-        user:'root',
-        password: '200123Perromarzo_',
-        database: 'database_links'
+        host: 'brmim6sbh3wjzb8vsnc4-mysql.services.clever-cloud.com',
+        user:'ujwhj4sh36r7qc1t',
+        password: 'PVL5x8HLLcFGI9JWYNQK',
+        database: 'brmim6sbh3wjzb8vsnc4'
     }
 };
